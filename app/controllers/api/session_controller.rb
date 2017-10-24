cbf20c19c86f0bfce19205a331155b584b339fbc
Christopher Lew <chrislew@lewcentral.com>
2017-10-24 12:06:16 -0400

    added users and session controller

---
+++ app/controllers/api/session_controller.rb
@@ -0,0 +1,25 @@
+class Api::SessionsController < ApplicationController
+
+  def create
+
+    @user = User.find_by_credentials(params[:user][:username],
+                                    params[:user][:password])
+
+    if @user
+      login(@user)
+      render '/api/users/show'
+    else
+      render json: ["Invalid username/password combination"], status: 401
+    end
+  end
+
+  def destroy
+    if logged_in?
+      logout
+      render json: {}, status: 200
+    else
+      render json: ["No current user!"], status: 404
+    end
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string           not null
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
   validates :email, :username, :session_token, presence: true, uniqueness: true
   validates :password_digest, presence: true
