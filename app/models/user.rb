2e41d00982dcc1577a930ff558479763c68e5790
Christopher Lew <chrislew@lewcentral.com>
2017-10-24 11:49:54 -0400

    created user model

---
+++ app/models/user.rb
@@ -0,0 +1,51 @@
+class User < ApplicationRecord
+  validates :email, :username, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+
+  attr_reader :password
+
+  after_initialize :ensure_session_token
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil unless user
+    user.is_password?(password) ? user : nil
+  end
+
+  def reset_session_token!
+    generate_unique_session_token
+    save!
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    generate_unique_session_token unless self.session_token
+  end
+
+  def create_new_session_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def generate_unique_session_token
+    self.session_token = new_session_token
+
+    while User.find_by(session_token: self.session_token)
+      self.session_token = new_session_token
+    end
+
+    self.session_token
+  end
+
+end
