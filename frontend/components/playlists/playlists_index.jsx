7a97563b68dd3e39fa2e82b4222f0e7d7b062384
Christopher Lew <chrislew@lewcentral.com>
2017-10-30 15:59:42 -0400

    added styling to new playlist modal

--- frontend/components/playlists/playlists_index.jsx
+++ frontend/components/playlists/playlists_index.jsx
@@ -4,6 +4,23 @@ import  PlaylistIndexItem  from './playlists_index_item';
 import NewPlaylist from './new_playlist';
 import Modal from 'react-modal';
 
+const customStyles ={
+  overlay : {
+      backgroundColor: 'rgba(25,20,20,.7)'
+  },
+  content : {
+    border: 'none',
+    height: '100%',
+    width: '100%',
+      top                   : '0%',
+      left                  : '0%',
+      right                 : 'auto',
+      bottom                : 'auto',
+      backgroundColor: 'rgba(25,20,20,.7)'
+
+  }
+};
+
 class PlaylistIndex extends React.Component {
   constructor(props) {
     super(props);
@@ -44,19 +61,22 @@ class PlaylistIndex extends React.Component {
     return (
       <div id='playlist-index-container'>
         <div id='playlist-index-header-button'>
+          <div id='playlist-index-header-invis'></div>
           <h1 id='playlists-index-header'>Playlists</h1>
           <button id='playlists-index-new-playlist-button'
                   onClick={this.openModal}>New Playlist</button>
-                <Modal
-                isOpen={this.state.modalIsOpen}
+                <Modal isOpen={this.state.modalIsOpen}
                 onAfterOpen={this.afterOpenModal}
                 onRequestClose={this.closeModal}
-                contentLabel="new-playlist-modal">
-            <button onClick={this.closeModal}>Close</button>
-            <NewPlaylist currentUser={this.props.currentUser}
-                         createPlaylist={this.props.createPlaylist}
-                         closeModal={this.closeModal}
-                         fetchPlaylists={this.props.fetchPlaylists}/>
+                contentLabel="new-playlist-modal"
+                style={customStyles}>
+            <div id='modal-content'>
+              <button onClick={this.closeModal}><i className='material-icons'>clear</i></button>
+              <NewPlaylist currentUser={this.props.currentUser}
+                           createPlaylist={this.props.createPlaylist}
+                           closeModal={this.closeModal}
+                           fetchPlaylists={this.props.fetchPlaylists}/>
+            </div>
           </Modal>
 
         </div>
