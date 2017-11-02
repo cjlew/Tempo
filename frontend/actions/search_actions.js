6ff509e2e5d33445604ee7fc6d19844f8713fded
Christopher Lew <chrislew@lewcentral.com>
2017-11-02 11:37:21 -0400

    added action to clear song when searching

--- frontend/actions/search_actions.js
+++ frontend/actions/search_actions.js
@@ -1,12 +1,21 @@
 import * as APIUtil from '../util/search_api_util';
 
 export const RECEIVE_SEARCH_SONGS = 'RECEIVE_SEARCH_SONGS';
+export const CLEAR_SONGS = 'CLEAR_SONGS';
 
+
+export const clearSongs = () => ({
+  type: CLEAR_SONGS,
+});
 
 export const receiveSearchSongs= (songs) => ({
   type: RECEIVE_SEARCH_SONGS,
   songs
 });
+
+export const clearSongState = () => (dispatch) => (
+  dispatch(clearSongs())
+);
 
 export const search = (query) => (dispatch) => (
   APIUtil.search(query)
--- frontend/reducers/song/song_reducer.js
+++ frontend/reducers/song/song_reducer.js
@@ -1,5 +1,5 @@
 import { RECEIVE_SONGS } from '../../actions/song_actions';
-import { RECEIVE_SEARCH_SONGS } from '../../actions/search_actions';
+import { RECEIVE_SEARCH_SONGS, CLEAR_SONGS } from '../../actions/search_actions';
 import merge from 'lodash/merge';
 
 const songReducer = (state = {}, action) => {
@@ -9,6 +9,8 @@ const songReducer = (state = {}, action) => {
       return Object.assign({}, state, action.songs);
     case RECEIVE_SEARCH_SONGS:
       return Object.assign({}, action.songs);
+    case CLEAR_SONGS:
+      return {};
     default:
       return state;
   }
