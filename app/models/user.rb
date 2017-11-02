5613b1c6687c9a528867f2b3f605ac82120a66d4
Christopher Lew <chrislew@lewcentral.com>
2017-11-02 14:52:45 -0400

    updated backend for friending

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,12 +31,14 @@ class User < ApplicationRecord
 
   has_and_belongs_to_many :friends,
     class_name: 'User',
+    join_table: :friendships,
     foreign_key: :user_id,
     association_foreign_key: :friend_id
 
   has_many :friendships,
-    class_name:'Frienships',
+    class_name: 'Friendship',
     foreign_key: :user_id
+
 
   attr_reader :password
 
--- app/views/api/users/_user.json.jbuilder
+++ app/views/api/users/_user.json.jbuilder
@@ -1,3 +1,4 @@
 json.extract! user, :id, :username
 json.profile_picture asset_path(user.profile_picture.url(:original))
 json.playlist user.owned_playlists.pluck(:id)
+json.friends user.friends.pluck(:id)
